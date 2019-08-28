a7dfd135fff7496c8130ec9be4f3adf66f1fe819
Erwan Queffélec <airone.k@gmail.com>
2019-08-28 14:51:35 +0200

    Make install script super-fancy

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ COLOR_GREEN="\e[32m"
 COLOR_RED="\e[31m"
 COLOR_BLUE="\e[34m"
 BOLD="\e[1m"
+
 DONE="${BOLD}${COLOR_GREEN}✓${COLOR_RESET}"
 FAIL="${BOLD}${COLOR_RED}✗${COLOR_RESET}"
 WORK="${BOLD}${COLOR_BLUE}⥁${COLOR_RESET}"
@@ -15,46 +16,55 @@ MY_ZSH="${MY_ZSH_INSTALL_DIR}/zsh-config"
 
 MY_ZSH_REMOTE="https://github.com/3rwww1/zsh-config"
 
-echo "${BOLD}Welcome back, Erwan !${COLOR_RESET}"
-echo "${BOLD}Have you been a good boy and installed the correct dependencies ;) ?${COLOR_RESET}"
+bold() {
+  echo -e ${BOLD}$1${COLOR_RESET}
+}
 
-which git > /dev/null
-if [ "$?" != "0" ]
-then
-  echo "${FAIL} You need git installed to continue, sucker!"
-  exit 1
-else
-  echo "${DONE} git is installed! Maybe there's hope for you after all!"
-fi
+fail() {
+  echo -e ${FAIL} $1
+}
 
-which zsh > /dev/null
-if [ "$?" != "0" ]
-then
-  echo "${FAIL} You need zsh installed to continue, sucker!"
-  exit 1
-else
-  echo "${DONE} zsh is installed! Maybe there's hope for you after all!"
-fi
+good() {
+  echo -e ${DONE} $1
+}
+
+work() {
+  echo -e ${WORK} $1
+}
+
+check_dep() {
+  which $1 > /dev/null
+  if [ "$?" != "0" ]
+  then
+    fail "You need git installed to continue, sucker!"
+    exit 1
+  else
+    good "$1 is installed! Maybe there's hope for you after all!"
+  fi
+}
+
+bold "Hello, ${USER}!"
+bold "Have you been a good n3rd and installed the required dependencies? ;)"
+check_dep "git"
+check_dep "zsh"
 
 if [ ! -d "${MY_ZSH_INSTALL_DIR}" ]
 then
-  echo "${WORK} Creating the installation directory"
+  work "Creating the installation directory"
   mkdir -p ${MY_ZSH_INSTALL_DIR}
-  pushd ${MY_ZSH_INSTALL_DIR}
-    git clone ${MY_ZSH_REMOTE}
-  popd
+  pushd ${MY_ZSH_INSTALL_DIR} > /dev/null
+    git clone ${MY_ZSH_REMOTE} 2>&1 | sed 's/^/    /'
+  popd > /dev/null
 else
-  echo "${WORK} Pulling latest configuration..."
-  pushd ${MY_ZSH}
-    git pull ${MY_ZSH_REMOTE}
-  popd
+  work "Pulling latest configuration..."
+  pushd ${MY_ZSH} > /dev/null
+    git pull ${MY_ZSH_REMOTE} 2>&1 | sed 's/^/    /'
+  popd > /dev/null
 fi
 
-pushd ${MY_ZSH}
+pushd ${MY_ZSH} > /dev/null
   ln -sf ${MY_ZSH}/zshrc ~/.zshrc
   ln -sf ${MY_ZSH}/zshenv ~/.zshenv
-  exec zsh
-popd
+popd > /dev/null
 
-echo "${DONE} You're all set! Come back to me if you need anything ;)"
-
+good "You're all set! Come back to me if you need anything ;)"
