bb2457baa06471da469ffbcc7965390cfe5b9910
Erwan Queffélec <airone.k@gmail.com>
2019-08-28 15:22:13 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -11,10 +11,6 @@ DONE="${BOLD}${COLOR_GREEN}✓${COLOR_RESET}"
 FAIL="${BOLD}${COLOR_RED}✗${COLOR_RESET}"
 WORK="${BOLD}${COLOR_BLUE}⥁${COLOR_RESET}"
 
-MY_ZSH_INSTALL_DIR=${MY_ZSH_INSTALL_DIR:-"${HOME}/Code/tools/"}
-MY_ZSH="${MY_ZSH_INSTALL_DIR}/zsh-config"
-
-MY_ZSH_REMOTE="https://github.com/3rwww1/zsh-config"
 
 bold() {
   echo -e ${BOLD}$1${COLOR_RESET}
@@ -36,35 +32,49 @@ check_dep() {
   which $1 > /dev/null
   if [ "$?" != "0" ]
   then
-    fail "You need git installed to continue, sucker!"
+    fail "You need $1 installed to continue, sucker!"
     exit 1
   else
     good "$1 is installed! Maybe there's hope for you after all!"
   fi
 }
 
+MY_TOOLS_DIR=${MY_TOOLS_DIR:-"${HOME}/Code/tools/"}
+MY_ZSH="${MY_TOOLS_DIR}/zsh-config"
+MY_ZSH_REMOTE="https://github.com/3rwww1/zsh-config"
+
 bold "Hello, ${USER}!"
 bold "Have you been a good n3rd and installed the required dependencies? ;)"
 check_dep "git"
 check_dep "zsh"
 
-if [ ! -d "${MY_ZSH_INSTALL_DIR}" ]
+if [ ! -d "${MY_TOOLS_DIR}" ]
 then
-  work "Creating the installation directory"
-  mkdir -p ${MY_ZSH_INSTALL_DIR}
-  pushd ${MY_ZSH_INSTALL_DIR} > /dev/null
+  work "Creating the tools directory..."
+  mkdir -p ${MY_TOOLS_DIR}
+fi
+
+if [ ! -d "${MY_ZSH}" ]
+then
+  work "Cloning repository..."
+  pushd ${MY_TOOLS_DIR} > /dev/null
     git clone ${MY_ZSH_REMOTE} 2>&1 | sed 's/^/    /'
   popd > /dev/null
 else
   work "Pulling latest configuration..."
   pushd ${MY_ZSH} > /dev/null
+    git checkout master 2>&1 | sed 's/^/    /'
     git pull ${MY_ZSH_REMOTE} 2>&1 | sed 's/^/    /'
   popd > /dev/null
 fi
 
-pushd ${MY_ZSH} > /dev/null
-  ln -sf ${MY_ZSH}/zshrc ~/.zshrc
-  ln -sf ${MY_ZSH}/zshenv ~/.zshenv
-popd > /dev/null
+work "Installing..."
+ln -sf ${MY_ZSH}/zshrc ${HOME}/.zshrc
+ln -sf ${MY_ZSH}/zshenv ${HOME}/.zshenv
+if [ "${SHELL}" == "zsh" ]
+then
+  source ${HOME}/.zshenv
+  source ${HOME}/.zshrc
+fi
 
 good "You're all set! Come back to me if you need anything ;)"
